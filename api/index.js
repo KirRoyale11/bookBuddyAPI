1addc936fa9665c06c5d76520c9e1b309a7f3712
Kirsti Esch <miss.k.esch@gmail.com>
2024-08-29 12:36:59 -0400

    Authorization routing

--- api/index.js
+++ api/index.js
@@ -1,7 +1,36 @@
 const express = require("express");
-
 const apiRouter = express.Router();
 const jwt = require("jsonwebtoken");
+const { getUserById } = require("../db/users");
+
+apiRouter.use(async (req, res, next) => {
+  const auth = req.header("Authorization");
+  const prefix = "Bearer ";
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    console.log(auth);
+    const token = auth.slice(prefix.length);
+    try {
+      console.log("Checking...");
+      const parsedToken = jwt.verify(token, process.env.JWT_SECRET);
+      const id = parsedToken && parsedToken.id;
+
+      if (id) {
+        req.user = await getUserById(id);
+        console.log(req.user);
+        next();
+      }
+    } catch (error) {
+      next(err);
+    }
+  } else {
+    next({
+      name: "AuthHeaderError",
+      message: "Auth Header must start with 'Bearer'",
+    });
+  }
+});
 
 // register routes for requests that have form baseURL/api/books
 apiRouter.use("/books", require("./books"));
