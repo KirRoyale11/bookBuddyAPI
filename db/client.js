171b3b0e50e63fafb1c2d55774197f2369fcaee4
Kirsti Esch <miss.k.esch@gmail.com>
2024-09-12 10:23:07 -0400

    db user

--- db/client.js
+++ db/client.js
@@ -5,6 +5,7 @@ const client = new pg.Client({
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD, // to set default password if a password was enabled in setup
+  user: process.env.DB_USER,
 });
 
 module.exports = client;
