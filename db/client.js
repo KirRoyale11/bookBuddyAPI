3926aea9f1e1eb3a5fafa387b31a10449cca8de6
Kirsti Esch <miss.k.esch@gmail.com>
2024-09-12 10:15:16 -0400

    deploy:

--- db/client.js
+++ db/client.js
@@ -4,7 +4,7 @@ const client = new pg.Client({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  //   password: process.env.DB_PASSWORD // to set default password if a password was enabled in setup
+  password: process.env.DB_PASSWORD, // to set default password if a password was enabled in setup
 });
 
 module.exports = client;
--- db/reservations.js
+++ db/reservations.js
@@ -1,18 +1,19 @@
 const client = require("./client");
 
-const createReservation = async ({ userId, booksId }) => {
+const createReservation = async ({ booksId, userId }) => {
   try {
-    const SQL = `INSERT INTO reservations(userId, booksId)
+    const SQL = `INSERT INTO reservations(booksid, userId)
     VALUES($1,$2)
     RETURNING *`;
     const {
       rows: [result],
-    } = await client.query(SQL, [userId, booksId]);
+    } = await client.query(SQL, [booksId, userId]);
     return result;
   } catch (err) {
     throw err;
   }
 };
+// await createReservation({ userId: req.user.id, booksId: updated.id });
 
 const getReservation = async (id) => {
   try {
@@ -27,8 +28,9 @@ const getReservation = async (id) => {
 };
 
 const getUsersReservations = async (userId) => {
+  console.log(userId);
   try {
-    const SQL = `SELECT reservations.id, books.title, books.description, books.coverimage, books.author FROM 
+    const SQL = `SELECT reservations.id, books.id, books.title, books.description, books.coverimage, books.author FROM 
     reservations JOIN books ON reservations.booksid = books.id AND userid=$1`;
 
     const { rows } = await client.query(SQL, [userId]);
